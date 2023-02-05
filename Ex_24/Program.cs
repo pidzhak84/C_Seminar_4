3706fa8575b10bfd3eb7917b77847c037463003c
pidzhak <pidzhak84@gmail.com>
2023-02-05 21:11:10 +0300

    Ex_24

---
+++ Ex_24/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+На вход число A и выдает сумму чисел от 1 до A (7->28, 4->10, 8->36)
+*/
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = Convert.ToInt32(Console.ReadLine());
+    return userData;
+}
+
+int getSumOfRange(int start, int end)
+{
+    int sum = 0;
+    for (int i = start; i <= end; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+void showData(string messageStart, int data)
+{
+    Console.Write(messageStart);
+    Console.Write(data);
+}
+int end = getUserData("Введите число А ");
+int sum = getSumOfRange(1, end);
+showData($"Сумма чисел от 1 до {end} = ", sum);
