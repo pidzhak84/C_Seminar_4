fd455bd3c0851063484f89f49d9abc9893f72616
pidzhak <pidzhak84@gmail.com>
2023-02-05 21:57:58 +0300

    Ex_26

---
+++ Ex_26/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 26. На вход число и на выходе выдает количество цифр в числе (456->3)
+*/
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = Convert.ToInt32(Console.ReadLine());
+    return userData;
+}
+int getNumberOfDigit(int number)
+{
+    int numberOfDigit = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        numberOfDigit++;
+    }
+    return numberOfDigit;
+}
+void showData(string messageStart, int data)
+{
+    Console.Write(messageStart);
+    Console.Write(data);
+}
+int number = getUserData("Введите число ");
+int numberOfDigit = getNumberOfDigit(number);
+showData($"В числе {number} количество цифр = ", numberOfDigit);
