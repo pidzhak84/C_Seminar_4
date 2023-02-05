cb6031979e27caa25df393175c820015d504e7da
pidzhak <pidzhak84@gmail.com>
2023-02-05 23:06:48 +0300

    Ex_30

---
+++ Ex_30/Program.cs
@@ -0,0 +1,33 @@
+﻿/* 
+Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+
+int[] getArrayRandom(int lenght)
+{
+    int[] arr = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+    return arr;
+}
+
+void showArray(int[] array)
+{
+    Console.Write($"[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1)
+        {
+            Console.Write($"{array[i]},");
+        }
+        else
+        {
+            Console.Write($"{array[i]}]");
+        }
+    }
+}
+
+int[] array = getArrayRandom(8);
+showArray(array);
