556ff0a393ef39ef045e133ce6ebec20c19011e3
pidzhak <pidzhak84@gmail.com>
2023-02-05 22:26:35 +0300

    Ex_28

---
+++ Ex_28/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 28. На вход число N и выдает произведение чисел от 1 до N (4-> 24)
+*/
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = Convert.ToInt32(Console.ReadLine());
+    return userData;
+}
+int getMultiplic(int start, int end)
+{
+    int multip = 1;
+    for (int i = start; i <= end; i++)
+    {
+        multip = multip*i;
+    }
+    return multip;
+}
+void showData(string messageStart, int data)
+{
+    Console.Write(messageStart);
+    Console.Write(data);
+}
+int end = getUserData("Введите число N ");
+int multip = getMultiplic(1, end);
+showData($"Произведение чисел от 1 до {end} = ", multip);
